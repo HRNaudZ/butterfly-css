9cfa49a46d0a1f18d99841cabd784892a10ea467
WOANA Yao Renaud <woanayr@gmail.com>
2018-06-30 12:47:24 +0000

    Delete anglais.sol
    This file is not included in the project !!!

--- css/anglais.sol
+++
@@ -1,9 +0,0 @@
-L'inde demeure ou reste l'épicentre de la soustraitance pour le moment Avec la chine qui rattrape progressivement, mais la situation evolue
-continuellement. Quand les salaires seront suffisamment élevé en Inde et en chine, la main d'oeuvre étrangère d'ailleurs sera aussi attractive / aléchante dans les pays soustraitant
-comme le sont présentement l'Inde et la Chine pour l'Europe , le Japon et les Etats-Unis.
-
-It is also known as personal or friendly letters. written to close friends or acquaintances (eg: penfriend, close relative of same age group such as cousins nephews, nieces)
-
-Semi-formal letters
-They are written to adult friends, personally known to you, or Unknown personns not in their capacity as officials
-They are also known
